d12f6057100f86e3295108a1b9470c3db4fe41f5
Ahmed Gabal <ahmed.gabal@evapharma.com>
2023-06-04 20:08:53 +0300

    feat: new updates

--- src/app/best-clean/best-clean.component.ts
+++ src/app/best-clean/best-clean.component.ts
@@ -16,4 +16,37 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y ]);
 export class BestCleanComponent {
   @Input() sliderElements!: any[];
   @Input() sliderTitle!: any;
+
+  fullSliderConfig: any = {
+    spaceBetween: 16,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    }
+  };
+  SecondSliderConfig: any = {
+    spaceBetween: 16,
+    slidesPerView: 1,
+    breakpoints: {
+      // when window width is >= 320px
+      390: {
+        slidesPerView: 1,
+        spaceBetween: 8,
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      992: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+      1400: {
+        slidesPerView: 3,
+      },
+    }
+  };
 }
